e34d7f24792c0451ddb901b40261f343040fd642
Hernan Sisi <hsisi@frba.utn.edu.ar>
2024-04-16 22:45:38 -0300

    creacion de servidor de memoria

--- memoria/src/main.c
+++ memoria/src/main.c
@@ -3,5 +3,23 @@
 int main() {
     int conexion;
 
+    t_log *logger = log_create("memoria.log", "memoria", true, LOG_LEVEL_INFO);
+	log_info(logger, "Iniciando Memoria...");
+    t_config *config = config_create("memory.config");
+    if (config == NULL)
+    {
+        log_error(logger, "No se leyo el archivo de configuracion");
+        exit(EXIT_FAILURE);
+    }
+	char *puerto = config_get_string_value(config, "PUERTO_ESCUCHA");
+
+    int socket_servidor_memoria = iniciar_servidor(puerto);
+	log_info(logger, "Listo para recibir al CPU");
+    int socket_cliente_cpu = esperar_cliente(socket_servidor_memoria, logger);
+    
+    int socket_cliente_kernel = esperar_cliente(socket_servidor_memoria, logger);
+    
+    int socket_cliente_io = esperar_cliente(socket_servidor_memoria, logger);
+
     return 0;
 }

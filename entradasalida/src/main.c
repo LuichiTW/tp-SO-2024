6b3d2a91e727e1336933bc3441c772865b37bba2
julianbidone <julian.bidonemail.com>
2024-05-24 20:44:55 +0000

    Hilo escucha IO

--- entradasalida/src/main.c
+++ entradasalida/src/main.c
@@ -1,21 +1,44 @@
 #include "main.h"
+
+struct timespec tiempo 
+
 int main() {
     t_log *logger = log_create("io.log", "io", true, LOG_LEVEL_INFO);
     log_info(logger, "Iniciando I/O...");
-    t_config *config = config_create("../io.config");
-    if (config == NULL) {
-        log_error(logger, "No se leyo el archivo de configuracion");
-        exit(EXIT_FAILURE);
-    }
-
+    cargar_config();
+    
     //cliente a memoria
-    char *ip_memoria = config_get_string_value(config, "IP_MEMORIA");
-    char *puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
     int conexion_memoria = crear_conexion(ip_memoria,puerto_memoria, "Hola soy el IO");
     //cliente a kernel
-    char *ip_kernel = config_get_string_value(config, "IP_KERNEL");
-    char *puerto_kernel = config_get_string_value(config, "PUERTO_KERNEL");
     int conexion_kernel = crear_conexion(ip_kernel,puerto_kernel, "Hola soy el IO");
-    char* algo = readline("> ");
+
+
+    int server_fd = iniciar_servidor(config_valores.puerto);
+
+    pthread_t io;
+    pthread_create(&io,NULL,(void*)manejarIO,(void*)server_fd);
+    pthread_detach(io);
+
+
     return 0;
+    ///////////////////////////////////////////////////////////////////
+}
+
+void manejarIO(int server_fd){
+    while(1){
+        int socketCliente = esperar_cliente(server_fd);
+
+        pthread_t t;
+        pthread_create(&t,NULL,(void*)manejarConexion,(void*)socketCliente);
+        pthread_detach(t);
+    }
+}
+
+void manejarConexion(int socketCliente){
+    int tipoInterfaz = recibir_operacion(socketCliente);
+
+    switch(tipoInterfaz){
+        case IO_GEN_SLEEP:
+        iO_GEN_SLEEP(n);
+    }
 }
--- entradasalida/src/main.h
+++ entradasalida/src/main.h
@@ -7,5 +7,37 @@
 #include <commons/config.h>
 #include <commons/log.h>
 #include <commons/error.h>
+#include <pthread.h>
 #include <readline/readline.h>
+#include <utils/conexiones/conexiones_servidor.c>
 #endif // !MAIN_IO
+
+
+void iO_GEN_SLEEP(n){
+    for (int i = 0; i < n; i++) {
+        nanosleep(&tiempo, NULL);
+    }
+}
+
+void cargar_config(void){
+    config = config_create("../io.config");
+    if (config == NULL) {
+        log_error(logger, "No se leyo el archivo de configuracion");
+        exit(EXIT_FAILURE);
+    }
+    char *tipo_interfaz = config_get_string_value(config,"TIPO_INTERFAZ");
+    int *tiempo_unidad_trabajo = config_get_int_value(config,"TIEMPO_UNIDAD_TRABAJO");
+    char *path_base_dialfs = config_get_string_value(config,"PATH_BASE_DIALFS");
+    int *block_size = config_get_int_value(config,"BLOCK_SIZE");
+    int *block_count = config_get_int_value(config,"BLOCK_COUNT");
+    int *retraso_compactacion = config_get_int_value(config,"RETRASO_COMPACTACION");
+    struct timespec tiempo = tiempo_unidad_trabajo;
+    //cliente a memoria
+    char *ip_memoria = config_get_string_value(config, "IP_MEMORIA");
+    char *puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
+    //cliente a kernel
+    char *ip_kernel = config_get_string_value(config, "IP_KERNEL");
+    char *puerto_kernel = config_get_string_value(config, "PUERTO_KERNEL");
+
+    struct timespec tiempo = tiempo_unidad_trabajo;
+}
